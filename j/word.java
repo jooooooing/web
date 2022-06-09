0ee046569a65127f1bff2c68026709d272712af4
jooooooing <leegun1018@naver.com>
2022-06-09 13:22:19 +0900

    갓지훈의 방법

--- j/word.java
+++ j/word.java
@@ -1,28 +1,27 @@
-package Review1;
+package j;
 
 public class Word {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Word word = new Word("Hello World");
-		System.out.println(word.isVowel(0)); //false
-		System.out.println(word.isVowel(1)); //true
+		System.out.println(word.isVowel(0)); // false
+		System.out.println(word.isVowel(1)); // true
 		System.out.println(word.isVowel2(0)); //
-		System.out.println(word.isVowel2(1)); 
-		System.out.println(word.isVowel2(2)); 
-		System.out.println(word.isVowel2(3)); 
-		System.out.println(word.isVowel2(4)); 
-		System.out.println(word.isVowel2(5)); 
-		System.out.println(word.isVowel2(6)); 
-		System.out.println(word.isVowel2(7)); 
-		System.out.println(word.isVowel2(8)); 
-		System.out.println(word.isVowel2(9)); 
-		System.out.println(word.isVowel2(10)); 
-		System.out.println("자음" + word.isConsonant(0)); 
-		System.out.println("자음" + word.isConsonant(1)); 
-		System.out.println("자음" + word.isConsonant(2)); 
-		System.out.println("자음" + word.isConsonant(3)); 
-		System.out.println("자음" + word.isConsonant(4)); 
-	
+		System.out.println(word.isVowel2(1));
+		System.out.println(word.isVowel2(2));
+		System.out.println(word.isVowel2(3));
+		System.out.println(word.isVowel2(4));
+		System.out.println(word.isVowel2(5));
+		System.out.println(word.isVowel2(6));
+		System.out.println(word.isVowel2(7));
+		System.out.println(word.isVowel2(8));
+		System.out.println(word.isVowel2(9));
+		System.out.println(word.isVowel2(10));
+		System.out.println("자음" + word.isConsonant(0));
+		System.out.println("자음" + word.isConsonant(1));
+		System.out.println("자음" + word.isConsonant(2));
+		System.out.println("자음" + word.isConsonant(3));
+		System.out.println("자음" + word.isConsonant(4));
 
 	}
 
@@ -38,20 +37,24 @@ public class Word {
 				|| letters.substring(i, i + 1).equals("e") || letters.substring(i, i + 1).equals("o")
 				|| letters.substring(i, i + 1).equals("u");
 	}
-	
+
 	public boolean isVowel2(int i) {
 		String vowel = "aeiou";
-		return vowel.contains(letters.substring(i, i+1));
+		return vowel.contains(letters.substring(i, i + 1));
 	}
-	
+
 	// i번째 글자가 모음인지 // a,e,i,o,u 제외한 나머지
 	public boolean isConsonant(int i) {
 		boolean isCon = true;
 		String vowel = "aeiou";
-		if (vowel.contains(letters.substring(i, i+1))) {
+		if (vowel.contains(letters.substring(i, i + 1))) {
 			isCon = false;
 		}
 		return isCon;
-	}	
+	}
+
+	public boolean isConsonant2(int i) {
+		return !isVowel2(i);
+	}
 
 }
